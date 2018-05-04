2a0eecd926a876efce2ee5a536e301995b127f86
salvador <salvadorc94>
2018-05-04 12:17:58 -0600

    Añadida función de favoritos.

--- app/src/main/java/com/example/salvador/contactlist/ContactAdapter.java
+++ app/src/main/java/com/example/salvador/contactlist/ContactAdapter.java
@@ -1,6 +1,7 @@
 package com.example.salvador.contactlist;
 
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
@@ -14,8 +15,11 @@ import java.util.ArrayList;
 
 public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ContactsViewHolder> {
 
-    //Arraylist de la clase contactos TODO: SOLO PARA PRUEBAS.
+    //Arraylist de la clase contactos TODO: ARRAY SOLO PARA PRUEBAS.
     private ArrayList<Contacts> contactos;
+
+    private static boolean fav = false;
+    private Context context;
 
 
     public static class ContactsViewHolder extends RecyclerView.ViewHolder{
@@ -24,7 +28,6 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.Contacts
         CardView cardview;
         TextView name;
         TextView number;
-        //TODO: configurar boton despues
         Button favorite;
 
 
@@ -37,12 +40,13 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.Contacts
             cardview = itemView.findViewById(R.id.card_view_contact_list);
             name = itemView.findViewById(R.id.text_view_name);
             number = itemView.findViewById(R.id.text_view_number);
+            favorite = itemView.findViewById(R.id.btn_fav);
 
         }
 
     }
 
-    public ContactAdapter(ArrayList<Contacts> contactos){this.contactos=contactos;}
+    public ContactAdapter(ArrayList<Contacts> contactos, Context context){this.contactos=contactos; this.context = context;}
 
 
     @NonNull
@@ -52,11 +56,35 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.Contacts
         return (new ContactsViewHolder(v));
     }
 
+    //TODO: REVISAR ADVERTENCIA EN FINAL INT POSITION
     @Override
-    public void onBindViewHolder(@NonNull ContactAdapter.ContactsViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final ContactsViewHolder holder, final int position) {
         //aquí se agregan los listeners a los botones.
         holder.name.setText(contactos.get(position).getName());
         holder.number.setText(contactos.get(position).getName());
+
+
+        //Conocer si es favorito y así modificar el botón.
+        if (contactos.get(position).isFavorite()) {
+            holder.favorite.setText(R.string.btn_full_star);
+        } else {
+            holder.favorite.setText(R.string.btn_empty_star);
+        }
+
+        //Listener de cada boton favorito de la lista.
+        holder.favorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isFav(position)) {
+                    holder.favorite.setText(R.string.btn_full_star);
+                    ((MainActivity) context).addFavourite(contactos.get(position));
+
+                } else {
+                    holder.favorite.setText(R.string.btn_empty_star);
+                    ((MainActivity) context).eraseFavourite(contactos.get(position).getName());
+                }
+            }
+        });
 
 
     }
@@ -65,8 +93,21 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.Contacts
     public int getItemCount() {
         return contactos.size();
     }
+    //Metodos para saber si el adapter muestra falso o verdadero y si el contacto esta en favoritos.
+    public void setTrue() {
+        fav = true;
+    }
+    public void setFalse() {
+        fav = false;
+    }
+    public boolean isOnFavS() {
+        return fav;
+    }
 
-
+    public boolean isFav(int position){
+        contactos.get(position).setFavorite(!contactos.get(position).isFavorite());
+        return contactos.get(position).isFavorite();
+    }
 
 
 }
--- app/src/main/java/com/example/salvador/contactlist/MainActivity.java
+++ app/src/main/java/com/example/salvador/contactlist/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.salvador.contactlist;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
@@ -13,15 +15,22 @@ public class MainActivity extends AppCompatActivity {
 
     RecyclerView rv;
     ContactAdapter adapter;
-    //para mientras
-    ArrayList<Contacts> contacts;
     LinearLayoutManager manager;
+
+    Button home;
+    Button fav;
+
+    //Array para mientras
+    ArrayList<Contacts> contacts,favcontacts;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        home = findViewById(R.id.btn_home);
+        fav = findViewById(R.id.btn_fav);
 
         rv = findViewById(R.id.contact_list);
         rv.setHasFixedSize(true);
@@ -31,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
 
         //SI EL ARRAY LIST ESTA LISTO
         contacts = new ArrayList<>();
+        favcontacts = new ArrayList<>();
+
         contacts.add(new Contacts("SALVADOR","71647405", "ASDASD", "asdasd", false));
         contacts.add(new Contacts("SALVADOR","71647405", "ASDASD", "asdasd", false));
         contacts.add(new Contacts("SALVADOR","71647405", "ASDASD", "asdasd", false));
@@ -40,10 +51,47 @@ public class MainActivity extends AppCompatActivity {
         contacts.add(new Contacts("SALVADOR","71647405", "ASDASD", "asdasd", false));
         contacts.add(new Contacts("SALVADOR","71647405", "ASDASD", "asdasd", false));
 
-        adapter = new ContactAdapter(contacts);
+        adapter = new ContactAdapter(contacts, this);
         rv.setAdapter(adapter);
     }
 
+    //Metodos onClick del menu
 
+    public void homebtn(View v){
+        adapter.setFalse();
+        home.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+        fav.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+        adapter = new ContactAdapter(contacts,v.getContext());
+        rv.setAdapter(adapter);
+    }
+    public void favbtn(View v){
+        adapter.setTrue();
+        home.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+        fav.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+        adapter = new ContactAdapter(favcontacts,v.getContext());
+        rv.setAdapter(adapter);
+    }
 
+    public void addFavourite(Contacts contacts) {
+        favcontacts.add(contacts);
+    }
+
+    public void eraseFavourite(String name) {
+        int counter=0;
+        for (Contacts contacts : favcontacts){
+            if (contacts.getName()== name){
+                break;
+            }
+
+            counter++;
+        }
+
+        favcontacts.remove(counter);
+
+        if (adapter.isOnFavS()){
+            adapter = new ContactAdapter(favcontacts,this);
+            rv.setAdapter(adapter);
+
+        }
+    }
 }
